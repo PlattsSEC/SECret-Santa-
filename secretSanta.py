c33af6df77f4f79dd8479def6f2f41c51cef74fa
JustinG721 <jus102u@gmail.com>
2016-12-05 01:55:09 -0500

    imported actual guests. refined algorithm

--- secretSanta.py
+++ secretSanta.py
@@ -1,36 +1,47 @@
-# secret santa boiii
-# poppin pills wit a poppin bitch boiiiiii
-
-# urBoi Justin Grizzle. in the hizzle. Don't get it fuckin
-#twizzled
-
-
-
 #Secret Santa Algorithm
 
 
 import random
 
 
-people = ['JustinTheAwesome', 'TylerTheBitch', 'DannyTheGay', 'AlexTheFuckboy',
-          'PrithajTheTurtle']
-dicManBaby = {}
+
+
+def parseEmails(guests):
+    fobj = open(guests, 'r')
+    data = fobj.read().decode("utf-8-sig").encode("utf-8")
+    emails = {}
+    doods = data.split('\n')
+    del doods[-1]
+
+    for i in doods:
+        p = i.split(',')
+        p[0] = p[0][1:-1]
+        emails[p[0]] = p[1] + '@plattsburgh.edu'
+
+    return emails
+
+
 
 def match(friends, dicManBaby):
     random.shuffle(friends)
-    #print friends
     for i in range(len(friends) - 1):
-        #print friends[i]
         dicManBaby[friends[i]] = friends[i + 1]
-    #print friends[-1]
-    #print friends[0]
     dicManBaby[friends[-1]] = friends[0]    
     return dicManBaby
+
+
             
-        
+
+guestFile = 'guestList.txt'
+a = parseEmails(guestFile)
+
+b = a.values()
+
+matches = {}
+
+c = match(b, matches)
+
+for key, value in c.iteritems():
+    print key, value
 
 
-a = match(people, dicManBaby)
-
-for key, value in a.iteritems():
-    print key,value
